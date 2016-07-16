b5b1513eb6ad83173760a157cec8470c837e98e5
Victor Laznik <victor.laznik@gmail.com>
2016-07-16 14:40:23 -0400

    updated seeds and migrated database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+quotes = ["You can call me John. You can store me in any variable you wish.","It should work. But it isn't actually going to work.","I had to pull it out of you like I'm an evil dentist or something.","I don’t need to remember, I have google.","It isn't a button, “Its a buTTEn","You're GOING to screw this up.", "How do models eat?","I go to him; I choke him slowly...DON'T DELETE MIGRATIONS!","I don't know what you're saying. But does everyone have this in their file?"]
+
+quotes.each do |quote|
+  Quote.create(description: quote)
+end
